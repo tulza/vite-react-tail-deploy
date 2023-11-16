01dba0ce14c1b8f0d3de51babc8a7569320af5d1
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-16 22:00:15 +1300

    update template

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import { useState } from "react";
-import "./App.css";
+import "./style/styles.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          fk
-        </a>
-        <a href="https://react.dev" target="_blank">
-          u
-        </a>
+      <div className="flex justify-center align-middle">
+        <h1> thisa template</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
